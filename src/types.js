adc0c29a00063c78152e0d12580f9ecbef727e0f
SATOSHI SENGA <196413632+satoshi19840417@users.noreply.github.com>
2025-08-11 17:55:14 +0900

    Add shared JSDoc types and integrate with Store context

---
+++ src/types.js
@@ -0,0 +1,57 @@
+/**
+ * @typedef {Object} Transaction
+ * @property {string} date - ISO 8601 date string of the transaction.
+ * @property {string} [description] - Short description of the transaction.
+ * @property {string} [detail] - Additional details such as payee or store name.
+ * @property {string} [memo] - Optional memo or note.
+ * @property {number} amount - Amount of the transaction. Negative for expenses.
+ * @property {string} [category] - Category assigned to the transaction.
+ */
+
+/**
+ * @typedef {Object} Rule
+ * @property {string} [pattern] - Simple pattern string to search in descriptions.
+ * @property {string} [regex] - Regular expression string for advanced matching.
+ * @property {string} [flags] - Flags for the regular expression.
+ * @property {string} [keyword] - Keyword used for matching when pattern/regex is absent.
+ * @property {string} category - Category to apply when rule matches.
+ */
+
+/**
+ * @typedef {Object} StoreState
+ * @property {Transaction[]} transactions - Imported transactions after applying rules.
+ * @property {Rule[]} rules - Reclassification rules defined by the user.
+ * @property {string|null} lastImportAt - ISO string for last import timestamp.
+ */
+
+/**
+ * @typedef {{ type: 'loadFromStorage' }} LoadFromStorageAction
+ */
+
+/**
+ * @typedef {{ type: 'importTransactions', payload?: Transaction[] }} ImportTransactionsAction
+ */
+
+/**
+ * @typedef {{ type: 'setRules', payload?: Rule[] }} SetRulesAction
+ */
+
+/**
+ * @typedef {{ type: 'applyRules' }} ApplyRulesAction
+ */
+
+/**
+ * @typedef {{ type: 'clearAll' }} ClearAllAction
+ */
+
+/**
+ * @typedef {(
+ *   | LoadFromStorageAction
+ *   | ImportTransactionsAction
+ *   | SetRulesAction
+ *   | ApplyRulesAction
+ *   | ClearAllAction
+ * )} StoreAction
+ */
+
+export {};
