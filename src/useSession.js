0877575a6ac580f36564daf1debce272bf7b6e3f
SATOSHI SENGA <196413632+satoshi19840417@users.noreply.github.com>
2025-08-13 09:49:17 +0900

    feat: add supabase client singleton

--- src/useSession.js
+++ src/useSession.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { supabase } from './supabaseClient';
+import { supabase } from './lib/supabaseClient.js';
 
 export function useSession() {
   const [session, setSession] = useState(null);
